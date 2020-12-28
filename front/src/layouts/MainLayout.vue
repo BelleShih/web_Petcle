e69b1ba9d4ffd12750bd224c3de6c6a152f0c1f7
BelleShih <belleshih0502@gmail.com>
2020-12-28 23:42:45 +0800

    update

--- front/src/layouts/MainLayout.vue
+++ front/src/layouts/MainLayout.vue
@@ -1,11 +1,11 @@
 <template>
   <q-layout>
     <q-header elevated class="bg-white h-100" height-hint="98">
-      <q-toolbar>
-        <q-toolbar-title>
+      <q-toolbar class="d-flex row wrap">
+        <q-toolbar-title class="col-xs-12 col-lg-3 q-mr-auto">
           <img id="logo" src="../assets/petcle-logo.png">
         </q-toolbar-title>
-        <q-tabs align="left">
+        <q-tabs class="col-xs-12 col-lg-6">
           <q-route-tab class="tab" to="/questions" label="毛孩大小事" />
           <q-route-tab class="tab" to="/news" label="最新消息" />
           <q-route-tab class="tab" to="/about" label="關於 Petcle" />
