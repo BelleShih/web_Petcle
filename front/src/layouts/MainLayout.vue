b02da2fd5fc89fffeb68a20a056697ade5bbadac
BelleShih <belleshih0502@gmail.com>
2020-12-28 16:32:27 +0800

    front basic build

---
+++ front/src/layouts/MainLayout.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-layout>
+    <q-header elevated class="bg-white h-100" height-hint="98">
+      <q-toolbar>
+        <q-toolbar-title>
+          <img id="logo" src="../assets/petcle-logo.png">
+        </q-toolbar-title>
+        <q-tabs align="left">
+          <q-route-tab class="tab" to="/questions" label="毛孩大小事" />
+          <q-route-tab class="tab" to="/news" label="最新消息" />
+          <q-route-tab class="tab" to="/about" label="關於 Petcle" />
+          <q-route-tab class="tab" to="/login" label="登入" />
+        </q-tabs>
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'MainLayout'
+}
+</script>
