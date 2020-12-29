9cd687ca7f5e8b249e90e485ec632037786c2e06
BelleShih <belleshih0502@gmail.com>
2020-12-29 22:25:14 +0800

    index版面調整

--- front/src/layouts/MainLayout.vue
+++ front/src/layouts/MainLayout.vue
@@ -1,15 +1,16 @@
 <template>
   <q-layout>
     <q-header elevated class="bg-white h-100" height-hint="98">
-      <div class="d-flex row wrap">
-        <div class="col-12 col-lg-6 row flex-center">
+      <div class="flex row wrap">
+        <div class="col-12 col-lg-5 row flex-left flex-sm-center">
           <img id="logo" src="../assets/petcle-logo.png">
         </div>
-        <q-tabs class="col-12 col-lg-6 ">
+        <q-tabs class="col-12 col-lg-7 ">
           <q-route-tab class="tab" to="/questions" label="毛孩大小事" />
           <q-route-tab class="tab" to="/news" label="最新消息" />
           <q-route-tab class="tab" to="/about" label="關於 Petcle" />
           <q-route-tab class="tab" to="/login" label="登入" />
+          <q-route-tab class="tab" to="/registered" label="註冊" />
         </q-tabs>
       </div>
     </q-header>
--- front/src/pages/Index.vue
+++ front/src/pages/Index.vue
@@ -1,17 +1,17 @@
 <template>
-  <q-page-container class="flex flex-center">
+  <q-layout-container class="flex flex-center">
     <div class="row fit wrap justify-center items-start" >
-      <div class="col-6 col-md-4 col-lg-3 q-pa-lg" >
-        <q-card class="my-card" style="height:300px; ">
+      <div class="col-6 col-md-4 col-lg-3 q-pa-sm" v-for="n in 5" :key="`xs-${n}`">
+        <q-card class="my-card" style="heigh:30px">
           <img src="~assets/01.jpg">
-          <div class="absolute-right" style="top:250px">
+          <div class="absolute-bottom-right">
             <q-btn flat round color="primary" icon="star" />
           </div>
         </q-card>
           <q-btn flat round color="red" icon="favorite" />
       </div>
     </div>
-  </q-page-container>
+  </q-layout-container>
 </template>
 
 <script>
