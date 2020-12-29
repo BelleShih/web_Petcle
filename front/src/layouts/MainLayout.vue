c7d887e43567c9f5bad8995243483b935eee8fa2
BelleShih <belleshih0502@gmail.com>
2020-12-29 16:36:29 +0800

    navbar 初步排版完成

--- front/src/layouts/MainLayout.vue
+++ front/src/layouts/MainLayout.vue
@@ -1,17 +1,17 @@
 <template>
   <q-layout>
     <q-header elevated class="bg-white h-100" height-hint="98">
-      <q-toolbar class="d-flex row wrap">
-        <q-toolbar-title class="col-xs-12 col-lg-3 q-mr-auto">
+      <div class="d-flex row wrap">
+        <div class="col-12 col-lg-6 row flex-center">
           <img id="logo" src="../assets/petcle-logo.png">
-        </q-toolbar-title>
-        <q-tabs class="col-xs-12 col-lg-6">
+        </div>
+        <q-tabs class="col-12 col-lg-6 ">
           <q-route-tab class="tab" to="/questions" label="毛孩大小事" />
           <q-route-tab class="tab" to="/news" label="最新消息" />
           <q-route-tab class="tab" to="/about" label="關於 Petcle" />
           <q-route-tab class="tab" to="/login" label="登入" />
         </q-tabs>
-      </q-toolbar>
+      </div>
     </q-header>
     <q-page-container>
       <router-view />
--- front/src/pages/Index.vue
+++ front/src/pages/Index.vue
@@ -1,10 +1,17 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="../assets/quasar-logo-full.svg"
-    >
-  </q-page>
+  <q-page-container class="flex flex-center">
+    <div class="row fit wrap justify-center items-start" >
+      <div class="col-6 col-md-4 col-lg-3 q-pa-lg" >
+        <q-card class="my-card" style="height:300px; ">
+          <img src="~assets/01.jpg">
+          <div class="absolute-right" style="top:250px">
+            <q-btn flat round color="primary" icon="star" />
+          </div>
+        </q-card>
+          <q-btn flat round color="red" icon="favorite" />
+      </div>
+    </div>
+  </q-page-container>
 </template>
 
 <script>
