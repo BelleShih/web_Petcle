8a36ca14166a4eff884cc740fed592cf12cbefc1
BelleShih <belleshih0502@gmail.com>
2021-01-04 23:36:10 +0800

    關於我們基本版型

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,18 +1,38 @@
 <template>
   <q-page>
     <q-layout-container>
-      <div class="flex column flex-center">
-        <div class="flex column flex-center" style="color:#C2B593;letter-spacing: 0.5rem;">
-          <img id="about-logo" src="../assets/petcle-logo.png">
-          療癒力就是毛孩的超能力
-        </div>
-        <div class="flex row flex-center" style="overflow:auto;margin-top:20px">
-          <img class="col-12 col-lg-6" src="https://picsum.photos/500/300/?random=1">
-          <div class="col-12 col-lg-6 about-title" >
-            <b>關於我們</b>
-            <div id="about-line"></div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, amet recusandae nesciunt qui animi autem mollitia dolore praesentium? Officiis dolorem quod quia maxime nihil fugiat iure praesentium, ipsum eos modi tempore, repellat dicta obcaecati, nostrum facere velit? Quis impedit ea hic ad veritatis ipsa repellat architecto. Maiores perspiciatis consequatur molestiae labore repudiandae modi quidem totam eius, velit deserunt. Fuga veritatis minima repellat nam similique architecto ullam ipsam rerum provident quam, corporis, impedit atque delectus. Non nemo totam sint quidem quaerat illo aliquam cum suscipit consectetur voluptate vero illum et recusandae, sequi dignissimos a! Repellendus numquam minima unde earum exercitationem quaerat.
+      <div class="flex column flex-center text-center" style="color:#C2B593;letter-spacing: 0.5rem;">
+        <img id="about-logo" src="../assets/petcle-logo.png">
+        <h7>療癒力就是毛孩的超能力</h7>
+      </div>
+      <div class="flex row flex-center about-title relative-position" style="margin-top:50px">
+          <div class="col-12 col-lg-5" style="height:100%">
+            <img class="about-img" src="~assets/aboutphoto.jpg">
           </div>
+          <div class="col-12 col-lg-7 about-text" style="">
+            <b style="font-size:1.5rem">關於我們  ABOUT US</b>
+            <div id="about-line"></div>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut iure alias impedit voluptate! Cumque voluptate exercitationem illo earum nemo beatae rerum, voluptates est. Ab obcaecati ex optio nihil? Aspernatur perspiciatis minima pariatur sit, labore aperiam in. Corrupti eum repellat omnis similique molestiae? Magnam, magni doloribus. Facilis inventore vel distinctio et nisi ab eius perspiciatis temporibus, quidem aperiam numquam earum omnis ipsum molestiae ducimus, officia voluptate quibusdam incidunt veniam harum sit alias tempora asperiores architecto.
+            </p>
+          </div>
+      </div>
+      <div id="about-cube"></div>
+      <div class="flex row flex-center" style="margin-top:50px">
+        <div class="col-12 col-lg-3 flex column flex-center q-pa-lg">
+          <q-icon name="star" color="primary" style="font-size:5rem"/>
+          <h6 class="about-h6">收藏喜愛的照片</h6>
+          <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque at aperiam nostrum fugit, distinctio aspernatur perferendis assumenda in, iste consectetur impedit omnis! Itaque, in eveniet laborum velit assumenda.</p>
+        </div>
+        <div class="col-12 col-lg-3 flex column flex-center q-pa-lg">
+          <q-icon name="star" color="primary" style="font-size:5rem"/>
+          <h6 class="about-h6">收藏喜愛的照片</h6>
+          <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque at aperiam nostrum fugit, distinctio aspernatur perferendis assumenda in, iste consectetur impedit omnis! Itaque, in eveniet laborum velit assumenda.</p>
+        </div>
+        <div class="col-12 col-lg-3 flex column flex-center q-pa-lg">
+          <q-icon name="star" color="primary" style="font-size:5rem"/>
+          <h6 class="about-h6">收藏喜愛的照片</h6>
+          <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque at aperiam nostrum fugit, distinctio aspernatur perferendis assumenda in, iste consectetur impedit omnis! Itaque, in eveniet laborum velit assumenda.</p>
         </div>
       </div>
     </q-layout-container>
