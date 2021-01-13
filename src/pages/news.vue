5689a74732c1bd49a54c4584050c04bfcf281887
BelleShih <belleshih0502@gmail.com>
2021-01-13 23:28:24 +0800

    毛孩大小事版型製作

---
+++ src/pages/news.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="q-pa-md flex justify-center" style="width:100%">
+    <div class="q-gutter-y-md" style="max-width: 800px">
+      <q-card class="flex flex-center">
+        <q-tabs
+          v-model="tab"
+          dense
+          class="text-grey"
+          active-color="primary"
+          indicator-color="primary"
+          align="justify"
+          narrow-indicator
+        >
+          <q-tab name="mails" label="Mails" />
+          <q-tab name="alarms" label="Alarms" />
+          <q-tab name="movies" label="Movies" />
+        </q-tabs>
+
+        <q-separator />
+
+        <q-tab-panels v-model="tab" animated>
+          <q-tab-panel name="mails">
+            <div class="text-h6">Mails</div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </q-tab-panel>
+
+          <q-tab-panel name="alarms">
+            <div class="text-h6">Alarms</div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </q-tab-panel>
+
+          <q-tab-panel name="movies">
+            <div class="text-h6">Movies</div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </q-tab-panel>
+        </q-tab-panels>
+      </q-card>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      tab: 'mails'
+    }
+  }
+}
+</script>
---
+++ src/pages/questions.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-page>
+    <q-layout-container>
+      <div class="flex row flex-center">
+        <div class="flex row col-4">
+          <q-btn flat style="color: #C2B593" label="飲食" />
+          <q-btn flat style="color: #C2B593" label="生活" />
+          <q-btn flat style="color: #C2B593" label="交友" />
+          <q-btn flat style="color: #C2B593" label="生病" />
+          <q-btn flat style="color: #C2B593" label="其他" />
+        </div>
+        <div>
+          <q-input borderless dense debounce="300" v-model="filter" icon="search">
+            <template v-slot:append id="search_q">
+              <q-icon id="search-icon" name="search" />
+            </template>
+            <q-btn unelevated rounded id="search-btn" label="搜尋" type="submit" />
+          </q-input>
+        </div>
+        <div>
+          <q-btn id="pedia_enter" rounded icon="emoji_objects" label="寵物百科" />
+        </div>
+      </div>
+    </q-layout-container>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
